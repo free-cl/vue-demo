da6969c708002e01c2ad2d151afd726b988bf8ce
chengliu <chengliu@csii.com.cn>
2018-04-03 10:30:22 +0800

    完成两个静态页面

--- src/components/my/activity.vue
+++ src/components/my/activity.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+  <div class="activity-box">
+    <div class="activity-menu">
+      <div class="item" :class="{'active': activityState == 1}" @click="selectActivityState(1)">已报名</div>
+      <div class="item" :class="{'active': activityState == 2}" @click="selectActivityState(2)">已结束</div>
+      <div class="item" :class="{'active': activityState == 3}" @click="selectActivityState(3)">已定制</div>
+    </div>
+    <div class="activity-list">
+      <div class="item" v-for="(item, index) in activityList" :key="index">
+        <div class="logo" :style="{height:activityLogoHeight,backgroundImage:'url('+item.logo+')'}"></div>
+        <div class="name overflow-ellipsis">{{item.name}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -7,12 +18,65 @@
 export default {
   data() {
     return {
-      showLogin: false
+      activityState: 1,
+      activityList: [],
+      activityLogoHeight: 0
+    }
+  },
+  mounted() {
+    this.selectActivityState(1)
+    // 活动图片宽高比为5:2
+    this.activityLogoHeight = (window.innerWidth - 40) * 0.4 + 'px'
+  },
+  methods: {
+    selectActivityState(state) {
+      this.activityState = state
+      this.activityList = [
+        { logo: 'http://who2o.cmbwh.mbcloud.com/statics/20171024113116780265.png', name: '象牙塔舞蹈' },
+        { logo: 'http://who2o.cmbwh.mbcloud.com/statics/20170914045055234020.png', name: '宇凰国学馆' }
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.activity-box {
+  background: #fff;
+  margin-top: 10px;
+  .activity-menu {
+    display: flex;
+    height: 40px;
+    line-height: 40px;
+    font-size: 15px;
+    border-bottom: 1px solid #D8D8D8;
+    .item {
+      width: 33.3%;
+      text-align: center;
+      &.active {
+        color: #48A0F2;
+        border-bottom: 2px solid #48A0F2;
+      }
+    }
+  }
+  .activity-list {
+    padding: 20px;
+    .item {
+      margin-bottom: 20px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+      .logo {
+        background-size: cover;
+        background-position: center;
+      }
+      .name {
+        background: #000;
+        color: #fff;
+        padding: 5px 10px;
+        text-align: center;
+      }
+    }
+  }
+}
 </style>
--- src/components/my/top.vue
+++ src/components/my/top.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
+    <div class="my-info">
+      <img class="headpic" :src="headpic"/>
+      <div class="name-row info-row">
+        <span>{{username}}</span>
+        <span class="seperate-line">|</span>
+        <span>领队</span>
+      </div>
+      <div class="score-row info-row">
+        <span>300积分</span>
+        <span class="seperate-line">|</span>
+        <span>5优惠券</span>
+      </div>
+    </div>
+
+    <div class="menu-box">
+      <div class="menu-item">
+        <div class="icon">
+          <img src="../../assets/team.png"/>
+        </div>
+        <div class="text">团队定制</div>
+      </div>
+      <div class="menu-item">
+        <div class="icon">
+          <img src="../../assets/notebook.png"/>
+        </div>
+        <div class="text">游记</div>
+      </div>
+      <div class="menu-item">
+        <div class="icon">
+          <img src="../../assets/ticket.png"/>
+        </div>
+        <div class="text">优惠券</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -7,12 +41,60 @@
 export default {
   data() {
     return {
-      showLogin: false
+      headpic: 'http://thirdwx.qlogo.cn/mmopen/ldQOPoAq6QWY2kgEnqlnLxq3Pz6KuaOgKcia4jf39n4a8sCYEZRibwcJicQ6heVAYxf5t3Z21X2l8AKq4FYJS7KGvGdsbkyCze6/132',
+      username: '张三'
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.my-info {
+  background: #48A0F2;
+  padding: 20px 0;
+  .headpic {
+    display: block;
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    margin: 0 auto;
+  }
+  .info-row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    &.name-row {
+      margin: 10px 0;
+    }
+    .seperate-line {
+      margin: 0 10px;
+    }
+  }
+}
 
+.menu-box {
+  padding: 20px 0;
+  background: #fff;
+  display: flex;
+  .menu-item {
+    width: 33.3%;
+    .icon {
+      width: 46px;
+      height: 46px;
+      border-radius: 50%;
+      background: #48A0F2;
+      padding: 10px;
+      margin: 0 auto 10px;
+      img {
+        width: 26px;
+        height: 26px;
+        display: block;
+      }
+    }
+    .text {
+      text-align: center;
+    }
+  }
+}
 </style>
